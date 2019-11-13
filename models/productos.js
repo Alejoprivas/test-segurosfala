c4047fdf82dd6e66113e7c07fa4667f22b5763ec
Alejandro <alejoprivas@gmail.com>
2019-11-13 18:03:13 -0300

    added function to get closest rule

--- models/productos.js
+++ models/productos.js
@@ -22,8 +22,8 @@ class Product{
 
 
     testProduct = async ()=>{
-        console.log(await this.sellNewProduct("Cobertura",5,5))
-
+        console.log(await this.simulateProductbehavior(5))
+        
     };
     getAllProducts = async ()=>{
         let productos  = JSON.parse( await readFileAsync(dataPath,'utf8'));
@@ -32,7 +32,6 @@ class Product{
     
     getProductByName = async(name)=>{
         let productos = JSON.parse( await readFileAsync(dataPath,'utf8'));
-        console.log("searching")
         let selectedProduct = productos.find((product)=>{
             return product.nombre;
         });
@@ -46,18 +45,92 @@ class Product{
             nombre: name,
             sellIn: sellIn,
             price:price,
-             defaultRules:[
+             rules:[
             {action:"add",setpricestep:0,daysremaining:11},
             {action:"add",setpricestep:0,daysremaining:0}
         ]
         }
         productos.push(productToSell);
-        console.log(productos)
+
+        await writeFileAsync(dataPath,JSON.stringify(productos)) 
     }
+    simulateProductbehavior = async (days)=>{ 
+        let productos = await this.getAllProducts();
+        
+        let events = productos.map((product)=>{
 
+            //console.log(product);
+            //let value = product.rules.reduce(iterateOverRules,product);
+            
 
+            let myrule = product.rules.reduce((prev, curr)=>{ 
+                return (Math.abs(curr.daysremaining - product.sellIn) < Math.abs( prev.daysremaining - product.sellIn )? curr: prev);
+              
+              });
+              
+
+              console.log(product.sellIn);
+              console.log(myrule);
+              //let getCorrectRule = rulesRemainingDay.reduce()
+             
+             //value.sellIn--;
+            
+            //console.log(value);
+            
+            let suituableRule = product.rules.reduce((acc,currentElement,output)=>{
+                if(currentElement.daysremaining >= acc ){
+
+                };
+
+            },{});
+
+        })
+    }
     
+
      
 }
 
+/*
+var closest = 
+*/
+let iterateOverRules = (product,currentElement,output)=>{   
+    
+     let evaluatedProduct= applyRules(currentElement,product)
+    
+    accumulator = evaluatedProduct; 
+    return accumulator
+} 
+let applyRules = (rule,product)=>{
+    if(product.sellIn >= rule.daysremaining){
+        if(rule.action === "add" && product.price < 100){
+            product.price = product.price + rule.setpricestep;
+        }
+        if(rule.action === "substract" && product.price > 0){
+            product.price = product.price - rule.setpricestep;
+        } 
+    }
+
+    return product;
+}
+
 module.exports = Product
+
+/**
+ * 
+ *             product.rules.map((rule)=>{ 
+                if(product.sellIn >= rule.daysremaining){
+                    if(rule.action == "add" && product.price > 100){
+                        product.price = product.price + parseInt(rule.setpricestep)
+                    }
+                    if(rule.action == "substract" && product.price < 0){
+                        product.price = product.price - parseInt(rule.setpricestep)
+                    }
+                    console.log("apply logic")                    
+                    console.log(product)                    
+                }
+                if(product.sellIn >= 0 ){
+                    product.sellIn--;
+                }
+            })
+ * **/
